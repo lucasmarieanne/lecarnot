e2fd698f0b1624bf75a61563b2e6e99bc23d1832
Lucas MARIE-ANNE <pro.lucasmarieanne@gmail.com>
2025-11-20 15:27:47 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le CARNOT - Site Officiel</title>
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/import.css">
+</head>
+<body>
+
+    <main class="big-container">
+        <header>
+            <div class="header-container">
+                <div class="header-bg">
+                    <div class="header-logo">
+                        <a href="">Le CARNOT</a>
+                    </div>
+                    <nav>
+                        <ul class="nav-list">
+                            <li><a href="">Accueil</a></li>
+                            <li><a href="">Nos produits</a></li>
+                            <li><a href="">Le point vente</a></li>
+                        </ul>
+                    </nav>
+                    
+                    <div class="nav-contact">
+                        <ul>
+                            <li><a href="" class="button-contact">Contactez-nous</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+
+        <section class="home">
+            <div class="home-container">
+
+                <div class="home-box">
+
+                    <div class="home-box-content">
+                        <!-- <h1> Le CARNOT <br> </h1> -->
+                        <h1>Votre Tabac <br> de Référence <br> à Challans</h1>
+                        <!-- <p>Tabac, vapotage, jeux & confiserie — avec prochainement un espace Presse pour compléter nos services.</p> -->
+                        <p>Tabac, vape, jeux & confiserie à Challans, portés par l'expérience d'un professionnel reconnu du secteur. <br> Découvrez prochainement notre nouvel espace Presse.</p>
+                        
+                        <div class="home-button">
+                            <a href="" class="button-more">En savoir plus</a>
+                            <a href="" class="button-home-contact">Contactez-nous</a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="home-box">
+                    <div class="home-box-content-img">
+                        <!-- <img src="./assets/images/homepage.jpg" alt="Image page d'Accueil"> -->
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="services">
+            <div class="service-container">
+                <h1>Nos produits</h1>
+                <p>Découvrez une sélection complète conçue pour répondre aux besoins du quotidien.
+Nous proposons un large choix de tabac, une gamme moderne de solutions de vapotage, ainsi qu'un espace dédié aux jeux et à la confiserie pour accompagner petits plaisirs et moments de détente.
+Très bientôt, nous enrichirons notre offre avec un rayon Presse, afin de vous offrir encore plus de services en un seul lieu.</p>
+            </div>
+        </section>
+
+        <section class="shop">
+            <div class="shop-container">
+                <div class="shop-box">
+                    <h1>Le point de vente</h1>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2998.8845271042314!2d-1.8841599233665016!3d46.84678457113022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805ab782f82c567%3A0x47adb0d9282d107f!2s34%20Rue%20Carnot%2C%2085300%20Challans!5e1!3m2!1sfr!2sfr!4v1763550455760!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+
+        </section>
+    </main>
+    
+</body>
+</html>
